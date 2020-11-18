fdc4463211380aa68f6b78e2b28a42fc2edfa66f
Your Name <mmuzasantuna@gmail.com>
2020-11-18 13:01:52 +0000

    remote repo created

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    Bedroom bedroom;
+    Guest guest;
+
+    @Before
+    public void setUp() {
+        bedroom = new Bedroom(25,4, "Double");
+        guest = new Guest("Jane");
+    }
+
+    @Test
+    public void roomHasNumber() {
+    assertEquals(25, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void roomHasCapacity() {
+    assertEquals(4, bedroom.getCapacity());
+    }
+
+    @Test
+    public void roomHasType() {
+    assertEquals("Double", bedroom.getType());
+    }
 }
